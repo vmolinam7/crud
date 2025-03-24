a722fd847a33f6f982824f1695108c4f5af624b3
vmolinam7 <victor.molina2017@uteq.edu.ec>
2025-03-24 12:57:06 -0500

    archivos estaticos

--- crud/settings.py
+++ crud/settings.py
@@ -137,6 +137,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
